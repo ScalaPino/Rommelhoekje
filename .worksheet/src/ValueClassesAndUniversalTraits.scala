c0c9b856f366dd20a53dcaedc1bf6f58c2bcfe3f
ScalaPino <vdenberg+github@gmail.com>
2013-01-09 23:59:00 +0100

    Worksheet .scala copies remove

--- .worksheet/src/ValueClassesAndUniversalTraits.scala
+++
@@ -1,3 +0,0 @@
-object ValueClassesAndUniversalTraits {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(83); 
-  println("Welcome to the Scala worksheet")}
-}
--- .worksheet/src/org.kodejava.example.joda.FormattingDemo.scala
+++
@@ -1,5 +0,0 @@
-package org.kodejava.example.joda
-
-object FormattingDemo {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(102); 
-  println("Welcome to the Scala worksheet")}
-}
